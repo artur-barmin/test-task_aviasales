140fb2d8171ce23560b934d329aa1da39be1ab48
Artur <art.institoris@gmail.com>
2020-06-07 17:20:09 +0300

    Updates

---
+++ precache-manifest.988adcd4174a997a25262ea88a4791c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2d0407e4cb974ac1560589aa402579",
+    "url": "/test-task_aviasales/index.html"
+  },
+  {
+    "revision": "c434d91f6499a1082651",
+    "url": "/test-task_aviasales/static/css/main.14f26d8c.chunk.css"
+  },
+  {
+    "revision": "c3aa8fea165f8342da22",
+    "url": "/test-task_aviasales/static/js/2.662fc8ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-task_aviasales/static/js/2.662fc8ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c434d91f6499a1082651",
+    "url": "/test-task_aviasales/static/js/main.fd9b4dc9.chunk.js"
+  },
+  {
+    "revision": "c8abc901700b02ee092c",
+    "url": "/test-task_aviasales/static/js/runtime-main.88879e60.js"
+  },
+  {
+    "revision": "68ca57dc522eda771a31bb07bdd256b1",
+    "url": "/test-task_aviasales/static/media/checkmark.68ca57dc.svg"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/test-task_aviasales/static/media/logo_aviasales.0c089234.svg"
+  }
+]);
