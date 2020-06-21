ca069588f79ddbac1d2e778ad85cd27eed986138
Artur <art.institoris@gmail.com>
2020-06-21 14:16:44 +0300

    done: delay decorator

--- src/index.js
+++ src/index.js
@@ -22,8 +22,9 @@ class App extends Component {
       },
     }
   }
+  _timeoutBeforeSearch = 2000;
   render() {
-    let handleSearchParams = this.bufferingDecorator(this._timeoutBeforeSearch);
+    let handleSearchParams = bufferingDecorator(this, this._timeoutBeforeSearch);
     return (
       <div className='container'>
         <WaitingTimer />
@@ -44,38 +45,6 @@ class App extends Component {
       </div>
     )
   }
-  // -----------------------
-  _timeoutBeforeSearch = 2000;
-  // Это замыкание даёт время юзеру прокликать все нужные контролы,
-  // и только потом запросить билеты с сервера.
-  // Зачем: плохо дёргать сервер на каждое движение пользователя =>
-  // => надо поменьше писать в стейт, как вариант.
-  bufferingDecorator = (ms) => {
-    let buffer = Object.assign({}, this.state);
-    // Таймеры объяв-ся здесь для сброса первым делом, если это не 1й клик
-    let bufferTimer;
-    let runWaitingTimer = waitingTimerDecorator(ms);
-    return (e) => {
-      clearTimeout(bufferTimer);
-      // Перезапись буфера новым состоянием контролов на момент клика
-      if (e.currentTarget.classList.contains('tabs')) {
-        buffer.sort = e.target.dataset.sorter;
-        highlightTabs(e);
-      } else {
-        buffer.filters = this.getCorrectFilters(e);
-        highlightBuffered(e, ms)
-      }
-      // запуск анимации ожидания
-      runWaitingTimer();
-      // запуск таймера записи буфера
-      bufferTimer = setTimeout(() => {
-        this.setState(() => buffer);
-        // WARN: нужно ли удалять таймер в конце выполнения?
-        clearTimeout(bufferTimer);
-      }, ms);
-    }
-  }
-
   // Проверка (можно ли изменить состояние чекбокса) и возврат всех чекбоксов
   getCorrectFilters(e) {
     const FILTERS = this.state.filters;
@@ -108,22 +77,58 @@ class App extends Component {
     return st.filters;
   }
 }
-function waitingTimerDecorator(ms) {
-  let timer;
-  return () => {
-    clearTimeout(timer);
-    let bufferAnim = document.querySelector('.timeout');
-    let animTip = document.querySelector('.timeout__tip');
-    animTip.className = 'timeout__tip';
-    bufferAnim.className = 'timeout';
-    animTip.classList.add('timeout__tip_run');
-    bufferAnim.classList.add('timeout_run');
-    timer = setTimeout(() => {
-      animTip.classList.remove('timeout__tip_run');
-      bufferAnim.classList.remove('timeout_run');
-    }, ms);
+// Это замыкание даёт время юзеру прокликать все нужные контролы,
+// и только потом запросить билеты с сервера.
+// Зачем: плохо дёргать сервер на каждое движение пользователя =>
+// => надо поменьше писать в стейт, как вариант.
+// Буфер сохраняет последний клик и обновляет время ожидания
+const bufferingDecorator = (context, ms) => {
+  const delaySetState         = rebootableDelayDecorator(ms, null, updateState);
+  const runAnimationWhenDelay = rebootableDelayDecorator(ms, runTimerUI, stopTimerUI);
+  const BUFFER = Object.assign({}, context.state);
+  return (e) => {
+    // Сохранение в буфер состояния контролов на момент последнего клика
+    if (e.currentTarget.classList.contains('tabs')) {
+      BUFFER.sort = e.target.dataset.sorter;
+      highlightTabs(e);
+    } else {
+      BUFFER.filters = context.getCorrectFilters(e);
+      highlightBuffered(e, ms)
+    }
+    // запуск таймера и анимации перед записью буфера в стейт
+    runAnimationWhenDelay();
+    delaySetState(context, BUFFER);
   }
 }
+// 
+function rebootableDelayDecorator(ms, funcStart, funcEnd) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    if (funcStart) {
+      funcStart.call(this, ...args);
+    }
+    timer = setTimeout(() => funcEnd(...args), ms)
+  }
+}
+function updateState(context, newState) {
+  context.setState(() => newState);
+}
+function runTimerUI() {
+  let bufferAnim = document.querySelector('.timeout');
+  let animTip = document.querySelector('.timeout__tip');
+  animTip.className = 'timeout__tip';
+  bufferAnim.className = 'timeout';
+  animTip.classList.add('timeout__tip_run');
+  bufferAnim.classList.add('timeout_run');
+}
+function stopTimerUI() {
+  let bufferAnim = document.querySelector('.timeout');
+  let animTip = document.querySelector('.timeout__tip');
+  animTip.classList.remove('timeout__tip_run');
+  bufferAnim.classList.remove('timeout_run');
+}
+
 // ***Временная подсветка кликнутых чекбоксов***
 // Задача: мгновенный UI-фидбек на клик (переключить CSS-класс)
 // Вопрос: как связать удаление .buffered с обновлением state.filters?
