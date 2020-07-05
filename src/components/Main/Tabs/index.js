758f8740523d888d2b0d615fed81b9b121f6bebe
artur <art.institoris@gmail.com>
2020-07-05 08:59:20 +0300

    refactor(Tabs): delete useless click handler

--- src/components/Main/Tabs/index.js
+++ src/components/Main/Tabs/index.js
@@ -3,12 +3,9 @@ import './style.css'
 
 export default function Tabs(props) {
   return (
-    <ul className="tabs card" onClick={(e) => handleClick(e, props)}>
+    <ul className="tabs card" onClick={(e) => props.onClick(e)}>
       <li data-sorter="cheapest" className="tabs__item tabs__item_active">Самый дешевый</li>
       <li data-sorter="fastest" className="tabs__item">Самый быстрый</li>
     </ul>
   );
-}
-function handleClick(e, props) {
-  props.onClick(e);
 }
