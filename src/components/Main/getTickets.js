c457b1c39a73409369bd0c81eb6d38c5e2c4c06d
artur <art.institoris@gmail.com>
2020-07-05 20:21:35 +0300

    refactor(Main): separate infinite loop preventer from receiving tickets, while-loop instead for-loop

--- src/components/Main/getTickets.js
+++ src/components/Main/getTickets.js
@@ -2,18 +2,17 @@ import filterTickets from './filterTickets'
 import { sortByDuration, sortByPrice } from './sortTickets'
 
 export default async function getTickets(sort, filters) {
-  const origin = new URL('https://front-test.beta.aviasales.ru');
-  const entryAPI = new URL('/search', origin);
-  const searchAPI = new URL('/tickets?searchId=', origin);
+  const searchID = await getSearchID('https://front-test.beta.aviasales.ru/search');
 
-  const searchID = await getSearchID(entryAPI);
   console.group('получаем билеты, searchID:', searchID);
 
-  let tickets = await loadAllTickets(searchAPI + searchID);
+  let tickets = await loadAllTickets('https://front-test.beta.aviasales.ru/tickets?searchId=' + searchID);
   tickets = filterTickets(tickets, filters);
   tickets = (sort === "cheapest" ? sortByPrice : sortByDuration)(tickets);
 
   console.groupEnd();
+
+  // Для тестового задания достаточно вывода 5 билетов
   return tickets.slice(0, 5);
 }
 
@@ -25,15 +24,21 @@ async function getSearchID(url) {
 
 async function loadAllTickets(url) {
   const store = [];
-  // ATTENTION: safeCounter появился из-за непредсказуемо случавшегося бесконечного цикла.
-  for (let stop = false, safeCounter = 0; stop !== true && safeCounter < 100; safeCounter += 1) {
-    if (safeCounter >= 90) { console.warn('превышение кол-ва запросов, осталось', 100 - safeCounter) }
+  let isStopped = false;
+  // предохранитель от бесконечного цикла на случай неправильной работы сервера
+  let safeCounter = 0;
+  while (!isStopped) {
     const pack = await loadPack(url);
-    stop = pack.stop;
+    isStopped = pack.stop;
     store.push(...pack.tickets);
-    if (stop) {
+    if (isStopped) {
       console.log('поиск завершен! stop:', pack.stop, ', count of tickets:', store.length);
     }
+    if (safeCounter >= 100) {
+      console.warn('Непредвиденное превышение кол-ва запросов, завершение поиска');
+      isStopped = true;
+    }
+    safeCounter += 1;
   }
   return store;
 }
@@ -46,4 +51,3 @@ async function loadPack(searchSessionURL) {
   }
   return await req.json();
 }
-
