33809c22aecbf8585986f3ec486136e5510a520e
artur <art.institoris@gmail.com>
2020-07-05 23:55:31 +0300

    UI(WaitingTimer): change tip text to more obvious

--- src/components/WaitingTimer/index.js
+++ src/components/WaitingTimer/index.js
@@ -4,7 +4,7 @@ import './style.css'
 export default function WaintingTimer() {
   return (
     <>
-      <p className="timeout__tip">Ожидание ввода...</p>
+      <p className="timeout__tip">Хотите добавить параметры поиска?</p>
       <div className='timeout'></div>
     </>
   );
